956e0a48b66e1ff82550a2d9310c0c2e326f1c2d
dollars77 <slot.dollars77@gmail.com>
2026-02-07 17:29:01 +0700

    size hot image

--- src/app/[locale]/test/WebsiteCard.jsx
+++ src/app/[locale]/test/WebsiteCard.jsx
@@ -72,7 +72,7 @@ const WebsiteCard = ({ website }) => {
               height={100}
               src="/hot-icon3.png"
               alt="Description"
-              className="relative z-10 w-1/3 lg:w-1/2 zoominoutimgaka"
+              className="relative z-10 lg:w-18 zoominoutimgaka"
             />
           </div>
           // <Lottie
