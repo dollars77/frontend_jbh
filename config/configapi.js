5d39ad3d9ac47e22297e2e883c5a8a8926e237ed
dollars77 <slot.dollars77@gmail.com>
2026-02-07 17:04:31 +0700

    http to https

--- config/configapi.js
+++ config/configapi.js
@@ -2,7 +2,7 @@ const config = {
     
     // API_SERVER: "http://localhost:8081/",
     // API_WEBSOCKET: "ws://localhost:8001",
-    API_SERVER: "http://jbh-design.com/",
+    API_SERVER: "https://jbh-design.com/",
     API_WEBSOCKET: "ws://jbh-design.com",
 
 }
