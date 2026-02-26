016f6900c116fda65d6eb90bbd8f1e5ebc00826f
dollars77 <slot.dollars77@gmail.com>
2026-02-26 16:08:18 +0700

    add telegram link

--- src/app/[locale]/function/social_links.js
+++ src/app/[locale]/function/social_links.js
@@ -3,7 +3,7 @@
 export const social_links = {
   facebook: "https://www.facebook.com/profile.php?id=61588255703554",
   line: "https://lin.ee/YLEyXNC",
-  telegram: "https://lin.ee/YLEyXNC",
+  telegram: "https://t.me/jbh_design",
 };
 // console.log(await getSocialLinks());
 
